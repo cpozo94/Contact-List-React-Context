56835470eb716bd67c1a287afdd844776e95ef82
cpozo94 <cpg.948@gmail.com>
2023-03-18 10:44:07 +0000

    updating files

--- src/js/views/todo.jsx
+++ src/js/views/todo.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/todo.css";
 
 const Todo = () => {
     console.log("hola")
     return (
 
         <div>
-            <h1>todo list</h1>
+            <h1>Add new contact</h1>
+        <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">Full name</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Full name"/>
+        </div>
+        <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">Email</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter email"/>
+        </div>
+        <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">Phone</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter phone"/>
+        </div>
+        <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">Adress</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter adress"/>
+        </div>
+        <div className="mb-3">
+        <button type="button" className="btn btn-primary">save</button>
+        </div>
+
+
             <Link to="/">
-            <button>hola</button>
+            or get back to contact
+
             </Link>
         </div>
          
